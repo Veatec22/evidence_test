ae8d37748d11ea53a2b7bed04315f1b0406b4ce1
Veatec22 <wazagiel@outlook.com>
2025-06-30 23:46:09 +0200

    Add layout component and update spoti page
    - Introduced a new layout component in +layout.svelte for consistent page structure.
    - Updated index.md to include a title for the home page.
    - Recreated the spoti.md file with enhanced SQL queries and visualizations, replacing the deleted version.
    - Added a logo image to the static directory.

---
+++ pages/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import '@evidence-dev/tailwind/fonts.css';
+	import '../app.css';
+	import { EvidenceDefaultLayout } from '@evidence-dev/core-components';
+	export let data;
+</script>
+
+<EvidenceDefaultLayout {data} logo="/logo.png" title="test_title" builtWithEvidence={false} githubRepo="https://github.com/Veatec22/evidence_test" hideBreadcrumbs={true} >
+	<slot slot="content" />
+</EvidenceDefaultLayout>
